f76420a8b155fec69a9554aeba7cf142a59ab69a
Claire Howard <142757298+cbear243@users.noreply.github.com>
2024-03-20 16:22:25 +0000

    Wed, Mar 20, 2024, 9:22 AM -07:00

---
+++ css/js/breakout.js
@@ -0,0 +1,45 @@
+rulesBtn = document.getElementById('rules-btn')
+rules = document.getElementById('rules')
+closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+// Create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+}
+
+// Create Paddle properties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height -20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
+
+// Draw ball on canvas
+function drawBall() {
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+drawBall()
+
+
+// Rules open and close event handlers
+rulesBtn.addEventListener('click', () => {
+    rules.classList.add('show')
+})
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show')
+})
