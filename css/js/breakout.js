7bbf887f77128c5a306b6d2489f29fedc2b7e771
Claire Howard <142757298+cbear243@users.noreply.github.com>
2024-03-20 16:24:56 +0000

    Wed, Mar 20, 2024, 9:24 AM -07:00

--- css/js/breakout.js
+++ css/js/breakout.js
@@ -1,39 +1,39 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
-canvas = document.getElementById('canvas')
-ctx = canvas.getContext('2d')
+// canvas = document.getElementById('canvas')
+// ctx = canvas.getContext('2d')
 
-// Create ball properties
-ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    size: 10,
-    speed: 4,
-    dx: 4,
-    dy: -4,
-}
+// // Create ball properties
+// ball = {
+//     x: canvas.width / 2,
+//     y: canvas.height / 2,
+//     size: 10,
+//     speed: 4,
+//     dx: 4,
+//     dy: -4,
+// }
 
-// Create Paddle properties
-paddle = {
-    x: canvas.width / 2 - 40,
-    y: canvas.height -20,
-    w: 80,
-    h: 10,
-    speed: 8,
-    dx: 0,
-}
+// // Create Paddle properties
+// paddle = {
+//     x: canvas.width / 2 - 40,
+//     y: canvas.height -20,
+//     w: 80,
+//     h: 10,
+//     speed: 8,
+//     dx: 0,
+// }
 
-// Draw ball on canvas
-function drawBall() {
-    ctx.beginPath()
-    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true)
-    ctx.fillStyle = '#0095dd'
-    ctx.fill()
-    ctx.closePath()
-}
+// // Draw ball on canvas
+// function drawBall() {
+//     ctx.beginPath()
+//     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2, true)
+//     ctx.fillStyle = '#0095dd'
+//     ctx.fill()
+//     ctx.closePath()
+// }
 
-drawBall()
+// drawBall()
 
 
 // Rules open and close event handlers
